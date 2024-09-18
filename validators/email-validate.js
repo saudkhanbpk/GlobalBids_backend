201e7ae27fd9fb52e1bbbad81b69c8f440296800
saudkhan <saudkhantc@gmail.com>
2024-09-18 14:37:44 +0500

    changes email vaildation

--- validators/email-validate.js
+++ validators/email-validate.js
@@ -1,10 +1,5 @@
 export function validateEmail(email) {
-  const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  const domainPart = email.split("@")[1];
-  const consecutiveDotsRegex = /\.{2,}/;
-  if (consecutiveDotsRegex.test(domainPart)) {
-    return false;
-  }
-
+  const emailRegex =
+    /^(?![.\-_])([a-zA-Z0-9._-]+)(?<![.\-_])@(?!(?:-|\.)[a-zA-Z0-9-]+)[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
   return emailRegex.test(email);
 }
