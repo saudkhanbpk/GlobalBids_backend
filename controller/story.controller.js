2d0af3cddf397043e65b3287b43deab5eade300f
saudkhan <saudkhantc@gmail.com>
2024-09-23 12:19:48 +0500

    added: add comment functionality

--- controller/story.controller.js
+++ controller/story.controller.js
@@ -72,6 +72,7 @@ export const addComment = async (req, res, next) => {
 
 export const getStoryDetails = async (req, res, next) => {
   const storyId = req.params.id;
+  const userId = req.user._id;
 
   try {
     const comments = await CommentModel.find({ story: storyId }).populate({
@@ -83,15 +84,43 @@ export const getStoryDetails = async (req, res, next) => {
     const totalLikes = await LikeModel.countDocuments({ story: storyId });
     const totalShares = await ShareModel.countDocuments({ story: storyId });
 
+    const hasLiked = await LikeModel.findOne({
+      story: storyId,
+      user: userId,
+    });
+
     return res.status(200).json({
       success: true,
       comments,
       totalComments,
       totalLikes,
-      totalShares
+      totalShares,
+      hasLiked: !!hasLiked,
     });
   } catch (error) {
-    console.log(error);
-    return next(new InternalServerError("Can't get all comments"));
+    return next(new InternalServerError("Can't get all comments and likes"));
+  }
+};
+
+export const toggleLike = async (req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user._id;
+
+  try {
+    const existingLike = await LikeModel.findOne({
+      story: id,
+      user: userId,
+    });
+
+    if (existingLike) {
+      await LikeModel.deleteOne({ _id: existingLike._id });
+      return res.status(200).json({ success: true, liked: false });
+    } else {
+      const newLike = new LikeModel({ user: userId, story: id });
+      await newLike.save();
+      return res.status(201).json({ success: true, liked: true });
+    }
+  } catch (error) {
+    return next(new InternalServerError());
   }
 };
--- routes/story.routes.js
+++ routes/story.routes.js
@@ -4,6 +4,7 @@ import {
   getStoryFeeds,
   addComment,
   getStoryDetails,
+  toggleLike,
 } from "../controller/story.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 import upload from "../config/multer.config.js";
@@ -19,5 +20,6 @@ storyRouter.post(
 storyRouter.get("/feeds", getStoryFeeds);
 storyRouter.post("/add-comment", authMiddleware, addComment);
 storyRouter.get("/get-details/:id", authMiddleware, getStoryDetails);
+storyRouter.get("/like/:id", authMiddleware, toggleLike);
 
 export default storyRouter;
