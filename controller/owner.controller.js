119450ee2a5c9ee62e13542c25facddfcc899794
saud khan <saudkhanbpk@gmail.com>
2024-10-11 12:46:01 +0500

    fixes: get event by user id

--- controller/owner.controller.js
+++ controller/owner.controller.js
@@ -87,8 +87,11 @@ export const getContractors = async (_req, res, next) => {
 };
 
 export const getHomeMaintenanceReminders = async (req, res, next) => {
+  const userId = req.user._id;
   try {
-    const reminders = await EventsModel.find().sort({ createdAt: -1 });
+    const reminders = await EventsModel.find({ user: userId }).sort({
+      createdAt: -1,
+    });
     return res.status(200).json({ success: true, reminders });
   } catch (error) {
     return next(new InternalServerError("can't get reminders"));
